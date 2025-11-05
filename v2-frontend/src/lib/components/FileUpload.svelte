c88fc2cd6b8b2156878c995ea05872a62ccc6c06
Sammii <kellow.sammii@gmail.com>
2025-11-05 21:33:07 +0000

    updating FE with progress bar

--- v2-frontend/src/lib/components/FileUpload.svelte
+++ v2-frontend/src/lib/components/FileUpload.svelte
@@ -17,7 +17,7 @@
 		uploadStatus = 'Uploading...';
 
 		try {
-			const response = await fetch('http://localhost:5000/upload', {
+			const response = await fetch('https://ai-voiceover-api.rss-reply.workers.dev/api/upload', {
 				method: 'POST',
 				body: formData
 			});
